f6f7610c6e6d86585215ae3cfc0ad835d4d345d5
DevTeles <rteles78@gmail.com>
2019-10-15 13:37:37 -0300

    Desafio01 - GoStack09

---
+++ index.js
@@ -0,0 +1,87 @@
+const express = require('express');
+const server = express();
+server.use(express.json());
+
+let numberOfRequests = 0;
+const projects = [];
+
+//Middleware que checa se o projeto existe
+function checkProjectExists(req, res, next) {
+  const { id } = req.params;
+  const project = projects.find(p => p.id == id);
+
+  if (!project) {
+    return res.status(400).json({ error: 'Project not found' });
+  }
+
+  return next();
+}
+
+// Middleware log de número de requisições
+function logRequests(req, res, next) {
+  numberOfRequests++;
+
+  console.log(`Número de requisições: ${numberOfRequests}`);
+
+  return next();
+}
+
+server.use(logRequests);
+
+// Retorna todos os projetos
+server.get('/projects', (req, res) => {
+  return res.json(projects);
+});
+
+// Cria o projeto.
+server.post('/projects', (req, res) => {
+  const { id, title } = req.body;
+
+  const project = {
+    id,
+    title,
+    tasks: []
+  };
+
+  projects.push(project);
+
+  return res.json(project);
+});
+
+//Altera titulo do projeto.
+server.put('/projects/:id', checkProjectExists, (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+
+  const project = projects.find(p => p.id == id);
+
+  project.title = title;
+
+  return res.json(project);
+});
+
+//Deleta um projeto específico.
+server.delete('/projects/:id', checkProjectExists, (req, res) => {
+  const { id } = req.params;
+
+  const projectIndex = projects.findIndex(p => p.id == id);
+
+  projects.splice(projectIndex, 1);
+
+  return res.send();
+});
+
+// Adiciona uma tarefa no projeto específico.
+server.post('/projects/:id/tasks', checkProjectExists, (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+
+  const project = projects.find(p => p.id == id);
+
+  project.tasks.push(title);
+
+  return res.json(project);
+});
+
+//porta que está escutando é a 3001 nesse caso.
+server.listen(3001);
